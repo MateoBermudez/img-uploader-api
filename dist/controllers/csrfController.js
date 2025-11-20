a16e7c787bae5f6e868466a16a485ee93c95637b
MateoBermudez <mateo.bermudez2910@gmail.com>
2025-11-19 22:14:39 -0500

    Remove dist from repo

--- dist/controllers/csrfController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CsrfController {
-    static async getCsrfToken(req, res) {
-        const token = req.csrfToken();
-        res.json({ csrfToken: token });
-    }
-}
-exports.default = CsrfController;
